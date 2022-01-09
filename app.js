aad2547d069723fdd4faee4fbd7661aea2e8eddd
Ontorok <nasir.amath07@gmail.com>
2022-01-09 14:43:06 +0600

    Day-08 complete

--- app.js
+++ app.js
@@ -1,45 +1,59 @@
-const people = [
-    { name: 'Wes', year: 1988 },
-    { name: 'Kait', year: 1986 },
-    { name: 'Irv', year: 1970 },
-    { name: 'Lux', year: 2015 }
-];
+const canvas = document.querySelector('canvas');
+const picker = document.querySelector('input[type="color"]');
 
-const comments = [
-    { text: 'Love this!', id: 523423 },
-    { text: 'Super good', id: 823423 },
-    { text: 'You are the best', id: 2039842 },
-    { text: 'Ramen is my fav food ever', id: 123523 },
-    { text: 'Nice Nice Nice!', id: 542328 }
-];
 
-// Some and Every Checks
-// Array.prototype.some() // is at least one person 19 or older?
-const some_people = people.some(p => {
-    const currentYear = new Date().getFullYear();
-    const age = currentYear - p.year;
-    return age >= 19;
-});;
-//console.log(some_people);
 
-// Array.prototype.every() // is everyone 19 or older?
-const every_people = people.every(p => {
-    const currentYear = new Date().getFullYear();
-    const age = currentYear - p.year;
-    return age >= 19;
-});;
-//console.log(every_people);
+const ctx = canvas.getContext('2d');
 
-// Array.prototype.find()
-// Find is like filter, but instead returns just the one you are looking for
-const filter_comment = comments.find(com => com.id === 823423);
-//console.log(filter_comment);
-// find the comment with the ID of 823423
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-// Array.prototype.findIndex()
-// Find the comment with this ID
-const filter_comment_index = comments.findIndex(com => com.id === 823423);
+ctx.strokeStyle = '#BADA55';
+ctx.lineWidth = 1;
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
 
-// delete the comment with the ID of 823423
-comments.splice(1, 1);
+let isDrawing = false, lastX = 0, lastY = 0, isIncrement = false, hue = 0;
 
+function draw(e) {
+    if (!isDrawing) return;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
+    if (hue >= 360) {
+        hue = 0;
+    }
+    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
+        isIncrement = !isIncrement;
+    }
+
+    if (!isIncrement) {
+        ctx.lineWidth++;
+    } else {
+        ctx.lineWidth--;
+    }
+}
+
+// picker.addEventListener('change', e => {
+//     ctx.strokeStyle = e.target.value;
+// });
+
+document.addEventListener('mousemove', draw);
+
+document.addEventListener('mousedown', function (e) {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+
+document.addEventListener('mouseup', function () {
+    isDrawing = false;
+
+});
+
+document.addEventListener('mouseout', function () {
+    isDrawing = false;
+});
