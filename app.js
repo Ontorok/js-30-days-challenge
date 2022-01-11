a711574ab2fa1282405aa81013fd091889c46c4e
Ontorok <nasir.amath07@gmail.com>
2022-01-11 13:21:52 +0600

    Day-10 complete

--- app.js
+++ app.js
@@ -1,59 +1,18 @@
-const canvas = document.querySelector('canvas');
-const picker = document.querySelector('input[type="color"]');
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
+let lastChecked;
 
-
-const ctx = canvas.getContext('2d');
-
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-ctx.strokeStyle = '#BADA55';
-ctx.lineWidth = 1;
-ctx.lineJoin = 'round';
-ctx.lineCap = 'round';
-
-let isDrawing = false, lastX = 0, lastY = 0, isIncrement = false, hue = 0;
-
-function draw(e) {
-    if (!isDrawing) return;
-    ctx.beginPath();
-    ctx.moveTo(lastX, lastY);
-    ctx.lineTo(e.offsetX, e.offsetY);
-    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
-    ctx.stroke();
-    [lastX, lastY] = [e.offsetX, e.offsetY];
-    hue++;
-    if (hue >= 360) {
-        hue = 0;
+function handleCheck(e) {
+    if (e.shiftKey && this.checked) {
+        let inBetween = false;
+        checkboxes.forEach(chekbox => {
+            if (chekbox === this || chekbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+            if (inBetween) chekbox.checked = true;
+        });
     }
-    if (ctx.lineWidth >= 100 || ctx.lineWidth <= 1) {
-        isIncrement = !isIncrement;
-    }
-
-    if (!isIncrement) {
-        ctx.lineWidth++;
-    } else {
-        ctx.lineWidth--;
-    }
+    lastChecked = this;
 }
 
-// picker.addEventListener('change', e => {
-//     ctx.strokeStyle = e.target.value;
-// });
-
-document.addEventListener('mousemove', draw);
-
-document.addEventListener('mousedown', function (e) {
-    isDrawing = true;
-    [lastX, lastY] = [e.offsetX, e.offsetY];
-});
-
-document.addEventListener('mouseup', function () {
-    isDrawing = false;
-
-});
-
-document.addEventListener('mouseout', function () {
-    isDrawing = false;
-});
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
